b344b5adbcf816a0ad2eda0db344687cf8b7511a
Darkin <aaarafat@yahoo.com>
2023-06-10 15:17:10 +0300

    add path aliasing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+
+export default defineConfig(() => {
+  return {
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, './src'),
+      },
+    },
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  };
 });
